6a447b76b45bf6dfaca6cd73c96acaf99f1fe3f6
Jasper Gough <jasper.gough@mrjaffesclass.com>
2024-01-12 11:44:37 -0800

    getters and setters

--- Tic-Tac-Toe/tictactoe/State.java
+++ Tic-Tac-Toe/tictactoe/State.java
@@ -10,5 +10,18 @@ public class State
     private String xName = "";
     private String oName = "";
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+    
+    public int getGameState() {
+        return this.gameState;
+    }
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+    public String getYName() {
+        return this.oName;
+    }
+    public void setXname(String oName) {
+        this.oName = oName;
+    }
 }
 
